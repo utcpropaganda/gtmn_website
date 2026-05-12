c7c027ce967f24fd645fd591a2460d123322deed
Christopher Sarzen <csarzen3@gatech.edu>
2026-05-12 15:05:07 -0400

    health check

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
 const checkHealthButton = document.getElementById("healthCheckButton");
 const testP = document.getElementById("testP")
 
-function checkHealth() {
-    testP.innerText = "OK"
+async function checkHealth() {
+    try {
+        const response = await fetch("http://localhost:8080/api/healthcheck");
+
+        testP.innerText = response.ok;
+    }
+    catch(err)
+    {
+        testP.innerText = false;
+    }
+
 }
 
 checkHealthButton.addEventListener("click", checkHealth, false);
